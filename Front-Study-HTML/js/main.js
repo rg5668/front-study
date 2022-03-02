04e70b99b97ba496d5ad53d562b9dbfce98767c9
KunHee Lim <rg5668@naver.com>
2022-03-02 16:36:38 +0900

    JS Study

--- Front-Study-HTML/js/main.js
+++ Front-Study-HTML/js/main.js
@@ -21,14 +21,71 @@ const moveBackToTop = () => {
 window.addEventListener('scroll', checkScroll);
 backToTop.addEventListener('click', moveBackToTop);
 
-// slide
+// ---------slide------------------------
 
-const transformPrev = () => {
+function transformNext(event) {
+    const slideNext = event.target;
+    const slidePrev = slideNext.previousElementSibling;
 
+    const classList = slideNext.parentElement.parentElement.nextElementSibling;
+    let activeli = classList.getAttribute('data-position');
+    const liList = classList.getElementsByTagName('li');
+
+    // 하나의 카드라도 왼쪽으로 이동했다면, 오른쪽으로 이동 가능
+    if(Number(activeli) < 0) {
+        activeli = Number(activeli) + 260;
+
+        // 왼쪽에 있던 카드가 오른쪽으로 갔다면, 다시 왼쪽으로 갈 수 있으므로 PREV 버튼 활성화
+        slidePrev.style.color = '#2f3059';
+        slidePrev.classList.add('slide-prev-hover');
+        slidePrev.addEventListener('click', transformPrev);
+
+        if (Number(activeli) === 0) {
+            slideNext.style.color = '#cfd8dc';
+            slideNext.classList.remove('slide-next-hover');
+            slideNext.removeEventListener('click', transformNext);
+        }
+    }
+
+    classList.style.transition = 'transform 1s';
+    classList.style.transform = 'translateX(' + String(activeli) + 'px)';
+    classList.setAttribute('data-position', activeli);
 }
 
-const slidePrevList = document.getElementsByClassName('slide-prev');
+function transformPrev(event) {
+    // 왼쪽으로 가는 함수
+    // target을 하면 이벤트의 요소를 가져올 수 있다. (click)
+    // nextElementSibling 그 아래 next
+    const slidePrev = event.target;
+    const slideNext = slidePrev.nextElementSibling;
 
+    // ul 태그 선택
+    const classList = slidePrev.parentElement.parentElement.nextElementSibling;
+    let activeli = classList.getAttribute('data-position');
+    let liList = classList.getElementsByTagName('li');
+
+    if(classList.clientWidth < (liList.length * 260 + Number(activeli))) {
+        activeli = Number(activeli) - 260;
+
+        if(classList.clientWidth < (liList.length * 260 + Number(activeli))) {
+            slidePrev.style.color = 'cfd8dc';
+            slidePrev.classList.remove('slide-prev-hover');
+            slidePrev.removeEventListener('click', transformPrev);
+        }
+
+        slideNext.style.color = '#2f3059';
+        slideNext.classList.add('slide-next-hover');
+        slideNext.addEventListener('click', transformNext);
+    }
+
+    classList.style.transition = 'transform 1s';
+    classList.style.transform = 'translateX(' + String(activeli) + 'px)';
+    classList.setAttribute('data-position', activeli);
+}
+
+
+const slidePrevList = document.getElementsByClassName('slide-prev');
+// 첫 로딩
 for (let i = 0; i < slidePrevList.length; i++) {
     // ul 태그 선택
     let classList = slidePrevList[i].parentElement.parentElement.nextElementSibling;
@@ -38,6 +95,7 @@ for (let i = 0; i < slidePrevList.length; i++) {
     // 카드가 ul 태그 너비보다 넘치면, 왼쪽(Prev) < 버튼을 활성화, 오른쪽(Next)는 현재 맨 첫카드 위치이므로 비활성화
     if (classList.clientWidth < (liList.length * 260)) {
         slidePrevList[i].classList.add('slide-prev-hover');
+        // 클릭 이벤트
         slidePrevList[i].addEventListener('click', transformPrev);
     } else {
         // 태그 삭제시, 부모 요소에서 removeChild 를 통해 삭제해야 함
